5ccc84e6f47f7f4b0b00e77f36c47ec23b6162e8
Timothy Akintilo <timtobi1@gmail.com>
2019-06-07 18:08:35 -0700

    table styling

--- scripts/plunder_table.js
+++ scripts/plunder_table.js
@@ -5,43 +5,43 @@ class PlunderTable {
     }
 
 init() {
-        console.log("HERE");
-        var ptable;
+    console.log("HERE");
+    var ptable;
 
-        //https://codepen.io/pj_/pen/aVEBOm
+    //https://codepen.io/pj_/pen/aVEBOm
 
-        // use es6 string templates to populate rows
-        const rowTemplate = (d) => {
-          return `
-          <td>${d.object}</td>
-          <td>${d.object_category}</td>
-          <td>${d.town}</td>
-          `;
-        };
+    // use es6 string templates to populate rows
+    const rowTemplate = (d) => {
+        return `
+        <td>${d.object}</td>
+        <td>${d.object_category}</td>
+        <td>${d.town}</td>
+        `;
+    };
 
 
-          var p = this.plunder.apply_filters();
+        var p = this.plunder.apply_filters();
 
-          // select viz and append table
-          const table = d3.select("#plunderTable").append("table");
+        // select viz and append table
+        const table = d3.select("#tableViewer").append("table");
 
-          // append headers
-         const header = table.append("thead")
-            .selectAll('th')
-            .data(["Object", "Object Category", "Town"])
-            .enter()
-            .append('th')
-            .text(d => d);
+        // append headers
+        const header = table.append("thead")
+        .selectAll('th')
+        .data(["Object", "Object Category", "Town"])
+        .enter()
+        .append('th')
+        .text(d => d);
 
-          // append rows with rowTemplate
-          const rows = table.append("tbody")
-            .selectAll("tr")
-            .data(p)
-            .enter()
-            .append("tr")
-            .html(rowTemplate);
+        // append rows with rowTemplate
+        const rows = table.append("tbody")
+        .selectAll("tr")
+        .data(p)
+        .enter()
+        .append("tr")
+        .html(rowTemplate);
 
-        }
+    }
 
 }
 
