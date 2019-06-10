f25b9d952237960dfe92a1b7331054bf4216f4c0
Ashrujit Ghoshal <ashrujit.ck@gmail.com>
2019-06-10 11:45:52 -0700

    fixed the dots problem

--- scripts/map_viz.js
+++ scripts/map_viz.js
@@ -125,17 +125,20 @@ class MapViz extends Component {
             .on("mouseout", () => this.tooltip.style("visibility", "hidden"));
 
             var luccadot = this.svg.selectAll(".luccadot").data([{town: "Lucca", lon: "10.5027", lat: "43.8429", ct: "150"}]);
+            var that=this
+            setTimeout(function lucca_dot(){
             luccadot = luccadot.enter()
                 .append("rect")
                 .attr("class", "luccadot")
-                .attr("x", (d) => this.initial_projection([d["lon"], d["lat"]])[0])
-                .attr("y", (d) => this.initial_projection([d["lon"], d["lat"]])[1])
+                .attr("x", (d) => that.true_projection([d["lon"], d["lat"]])[0])
+                .attr("y", (d) => that.true_projection([d["lon"], d["lat"]])[1])
                 .attr("width", 9)
                 .attr("height", 9)
                 .attr("fill","#FFFFFF")
-                .on("mouseover", (d) => this.lucca_tooltip.style("visibility", "visible") .html(d["town"] + ' (city center)'))
-                .on("mousemove", () => this.lucca_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px"))
-                .on("mouseout", () => this.lucca_tooltip.style("visibility", "hidden"));
+                .on("mouseover", (d) => that.lucca_tooltip.style("visibility", "visible") .html(d["town"] + ' (city center)'))
+                .on("mousemove", () => that.lucca_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px"))
+                .on("mouseout", () => that.lucca_tooltip.style("visibility", "hidden"));
+            },4000)
 
         // zoom in on load
         this.svg.selectAll("path")
@@ -161,12 +164,7 @@ class MapViz extends Component {
             .attr("cx", (d) => this.true_projection([d["lon"], d["lat"]])[0])
             .attr("cy", (d) => this.true_projection([d["lon"], d["lat"]])[1]);
             
-        luccadot.transition("luccadot_zoom")
-            .delay(this.load_transition_delay)
-            .duration(this.load_transition_duration)
-            .attr("cx", (d) => this.true_projection([d["lon"], d["lat"]])[0])
-            .attr("cy", (d) => this.true_projection([d["lon"], d["lat"]])[1]);
-
+       
         const radius = d3.scaleSqrt().domain([0, 200]).range([0, 10]);
         var legend = this.svg.append("g")
                              .attr("transform", `translate(${this.width-50},${this.height + 10})`)
